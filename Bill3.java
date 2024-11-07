afd4daec3bc20cda82a33af622e98708f7dd544f
noamabadi <noam.abadi@post.runi.ac.il>
2024-11-07 19:43:14 +0200

    1

--- Bill3.java
+++ Bill3.java
@@ -3,6 +3,16 @@ public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
 	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+		String name2 = args[1];
+		String name3 = args[2];
+
+		int pay = Integer.parseInt(args[3]);
+
+		// System.out.println(name1 + " " + name2 + " " + name3 + " " + pay);
+		double newPay = (double) pay / 3; 
+		newPay = Math.ceil(newPay); 
+		System.out.println("Dear" + " " + name3 + ", " +  name2 + "," + " " + "and" + " " + name1
+		 + ": " + "pay" + " " + newPay + " " + "Shekels" + " " + "each" + ".");
+	       
 	}
 }
--- FVCalc.java
+++ FVCalc.java
@@ -1,6 +1,14 @@
 // Computes the future value of a saving investment.
 public class FVCalc {
 	public static void main(String[] args){
-		// Replace this comment with your code
+		int money = Integer.parseInt(args[0]);
+		double rate = Double.parseDouble(args[1]);
+		int years = Integer.parseInt(args[2]);
+
+		int fValue = (int)(money * Math.pow(rate/100 + 1, years)); 
+
+		System.out.println("After " + years + " " + "years, " + money + "$ " + "saved " + "at " + rate + "%" + " will " + "yield " + fValue + "$");
+
+
 	}
 }
