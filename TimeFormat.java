f094ccc085fcd7e7a0243ad2a7c96dd1306c063b
noamabadi <noam.abadi@post.runi.ac.il>
2024-11-11 15:04:46 +0200

    n

--- TimeFormat.java
+++ TimeFormat.java
@@ -10,8 +10,22 @@ public class TimeFormat {
 		// It then concatenates the resulting string with the rightmost hour-digit,
 		// and then uses parseInt to cast the resulting string as an int.
 		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
-		// Does the same with the minutes part of the input.
+		
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
-        // Replace this comment with the rest of your code
+        
+		if (hours <= 12)
+		 { System.out.print(hours + ":"); } 
+		 else System.out.print(hours - 12 + ":");
+	
+		 if (minutes < 10) { System.out.print("0" + minutes); }
+		 else System.out.print(minutes);
+
+		 if (hours < 12) { System.out.println(" AM"); }
+		 else System.out.println(" PM");
+			
+		 }
+
+			
+		 
+
 	}
-}
